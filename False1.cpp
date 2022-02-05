33bee5d2ea3d508fc0ced384c37d0cfcd2774d7c
Oleg-Kolp <80969951+Oleg-Kolp@users.noreply.github.com>
2022-02-05 22:53:23 +0300

    Add files via upload

---
+++ False1.cpp
@@ -0,0 +1,25 @@
+﻿// False1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+bool b(0);//или b(false) логическая переменная
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int x;
+    cout << "  \t ЛОГИЧ. ПЕРЕМЕННАЯ(0 -> FALSE;иначе -> true)\n ";
+    cout << "  \t Введите число b = ";
+    cin >> x;
+    if (x)
+
+        cout << " \t b  is  true!" << std::endl;
+    else
+        cout << "\t b  is  false ! " << std::endl;
+    return 0;
+}
+
+
